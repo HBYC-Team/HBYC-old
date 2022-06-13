80f4777ef1de3a0047e88c02a78e9c0c2d06b3a9
hugocoding <mivck1012@gmail.com>
2022-06-13 08:08:52 +0800

    v0.0.2

--- app.py
+++ app.py
@@ -29,31 +29,44 @@ async def on_ready():
     for guild in client.guilds:
         print(guild.id, guild.name)
     print("------------------------")
+    await client.change_presence(activity=discord.Game(name="v0.0.2開發中！"))
     
 
 @client.slash_command(name = "load", description = "Load the Cog_Extension")
 async def load(
     ctx,
-    extension: Option(str, "Enter Extension Name")
+    extension: Option(str, "Enter Extension Name", choices=["chat", "event","music", "help", "user"]),
+    password: Option(str, "passwd")
 ):
-    client.load_extension(f"cmds.{extension}")
-    await ctx.respond(f"加載Cog: {extension} 完成!")
+    if password == passwd :
+        client.load_extension(f"cmds.{extension}")
+        await ctx.respond(f"加載Cog: {extension} 完成!")
+    else:
+        await ctx.respond("權限不足")
 
 @client.slash_command(name = "unload", description = "Un-Load the Cog_Extension")
 async def unload(
     ctx,
-    extension: Option(str, "Enter Extension Name")
+    extension: Option(str, "Enter Extension Name", choices=["chat", "event","music", "help", "user"]),
+    password: Option(str, "passwd")
 ):
-    client.unload_extension(f"cmds.{extension}")
-    await ctx.respond(f"關閉Cog: {extension} 完成!")
+    if password == passwd :
+        client.unload_extension(f"cmds.{extension}")
+        await ctx.respond(f"關閉Cog: {extension} 完成!")
+    else:
+        await ctx.respond("權限不足")
 
 @client.slash_command(name = "reload", description = "Re-Load the Cog_Extension")
 async def reload(
     ctx,
-    extension: Option(str, "Enter Extension Name")
+    extension: Option(str, "Enter Extension Name", choices=["chat", "event","music", "help", "user"]),
+    password: Option(str, "passwd")
 ):
-    client.reload_extension(f"cmds.{extension}")
-    await ctx.respond(f"重新加載Cog: {extension} 完成!")
+    if password == passwd :
+        client.reload_extension(f"cmds.{extension}")
+        await ctx.respond(f"重新加載Cog: {extension} 完成!")
+    else:
+        await ctx.respond("權限不足")
 
 
 
@@ -69,7 +82,7 @@ for filename in os.listdir("./cmds"):
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
-
+passwd = os.getenv("password")
 
 
 if __name__ == "__main__":
