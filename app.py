5ec6fb3033e4b112229f80f9967c55ef5ddb2a2e
hugocoding <mivck1012@gmail.com>
2022-07-01 21:14:31 +0800

    v0.0.4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@
 ************************************
 """
 import discord
-from discord.ext import commands
+from discord.ext import commands, bridge
 from discord.commands import slash_command, Option
 
 import json, os, time
@@ -18,22 +18,23 @@ from dotenv import load_dotenv
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Bot(prefix=">", intents=intents)
-
+client = bridge.Bot(command_prefix="c!", intents=intents)
+client.remove_command(name="help")
 
 @client.event
 async def on_ready():
     print("Bot Logined")
     print(client.user)
+    print(f"Logined at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
     print("------------------------")
     
     for guild in client.guilds:
         print(guild.id, guild.name)
-    
+    await client.change_presence(activity=discord.Game(name="測試中，功能可能無法使用"))
     print("------------------------")
     
 
-@client.slash_command(name = "load", description = "Load the Cog_Extension")
+@client.bridge_command(name = "load", description = "Load the Cog_Extension")
 async def load(
     ctx,
     extension: Option(str, "Enter Extension Name", choices=["chat", "event","music", "help", "user"]),
@@ -53,7 +54,7 @@ async def load(
         print("###Someone tried to load the cog###", PermessionDeniedFrom)
 
 
-@client.slash_command(name = "unload", description = "Un-Load the Cog_Extension")
+@client.bridge_command(name = "unload", description = "Un-Load the Cog_Extension")
 async def unload(
     ctx,
     extension: Option(str, "Enter Extension Name", choices=["chat", "event","music", "help", "user"]),
@@ -73,7 +74,7 @@ async def unload(
         print("###Someone tried to unload the cog###", PermessionDeniedFrom)
 
 
-@client.slash_command(name = "reload", description = "Re-Load the Cog_Extension")
+@client.bridge_command(name = "reload", description = "Re-Load the Cog_Extension")
 async def reload(
     ctx,
     extension: Option(str, "Enter Extension Name", choices=["chat", "event","music", "help", "user"]),
@@ -92,6 +93,28 @@ async def reload(
         await ctx.respond("密碼錯誤，如錯誤超過3次將直接把你列入使用黑名單(ban)，未來將無法使用HBYC")
         print("###Someone tried to reload the cog###", PermessionDeniedFrom)
 
+
+@client.bridge_command(name="custom", description="owner only")
+async def custom(ctx, presence: Option(str, "presence name"), password: Option(str, "passwd")):
+    PermessionDeniedFrom = (f"{ctx.author} at {ctx.author.guild.name}")
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+    if password == passwd :
+        await client.change_presence(activity=discord.Game(name=presence))
+        await ctx.respond(f"changed presence to {presence} done.")
+        print("/custom")
+        print(f"Presence changed:{presence}")
+        print("from", ctx.author.guild.name)
+        print("by:", ctx.author)
+        print(f"at {timestamp}")
+        print("-------")
+    
+    else:
+        await ctx.respond("密碼錯誤，如錯誤超過3次將直接把你列入使用黑名單(ban)，未來將無法使用HBYC")
+        print("### Someone tried to use custom presence as non-owner failed ###", PermessionDeniedFrom)
+        print("User:", ctx.author)
+        print(f"at {timestamp}")
+        print("-------")
 
 
 with open("config.json", mode="r", encoding="utf8") as config:
