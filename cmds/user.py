a8b4aaa4bd107f544123f3d357a2f5308d234fa0
hugocoding <mivck1012@gmail.com>
2022-07-01 20:16:10 +0800

    added message command

--- cmds/user.py
+++ cmds/user.py
@@ -1,6 +1,6 @@
 import discord
 from discord.commands import slash_command, Option
-from discord.ext import commands
+from discord.ext import commands, bridge
 from core.classes import Cog_Extension
 import random, time, asyncio, json
 
@@ -10,8 +10,9 @@ with open("config.json", mode="r", encoding="utf8") as config:
 
 
 class User(Cog_Extension):
-    @slash_command(name="avatar", description="取得一個使用者的頭像")
-    async def avatar(self, ctx, *, member: Option(discord.Member, required=False)):
+    @bridge.bridge_command(name="avatar", description="取得一個使用者的頭像", aliases=["av"])
+    @discord.option(name="member")
+    async def avatar(self, ctx, member: discord.Member):
         if not member:
             member = ctx.author
         userAvatar = member.avatar.url
@@ -26,8 +27,9 @@ class User(Cog_Extension):
         print("by", ctx.author)
         print("------")
     
-    @slash_command(name="report", description="將建議或錯誤回報給後台")
-    async def report(self, ctx, 回報內容: Option(str, "請在這裡填入錯誤或建議內容，會回報到作者的後台")):
+    @bridge.bridge_command(name="report", description="將建議或錯誤回報給後台", aliases=["rp"])
+    @discord.option(name="回報內容", type=str)
+    async def report(self, ctx, 回報內容: "請在這裡填入錯誤或建議內容，會回報到作者的後台"):
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         embed=discord.Embed(title="**回報資訊**", color=0x4b56a8)
         embed.set_author(name=f"回報用戶：{ctx.author}", icon_url=ctx.author.avatar.url)
@@ -42,7 +44,7 @@ class User(Cog_Extension):
         print("#############################")
         print("-------")
 
-    @slash_command(name="ping", description="看看機器人的跑速")
+    @bridge.bridge_command(name="ping", description="看看機器人的跑速")
     async def ping(self, ctx):
         await ctx.respond(f"🇵 🇴 🇳 🇬❗| 機器人延遲:{round(self.client.latency*1000)} (ms)")
         timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
@@ -53,15 +55,16 @@ class User(Cog_Extension):
         print("at", timestamp)
         print("-------")
 
-    @slash_command(name="clean", description="清除一定數量的訊息")
+    @bridge.bridge_command(name="clean", description="清除一定數量的訊息")
     @commands.has_permissions(administrator=True)
-    async def clean(self, ctx, num: int):
-        await ctx.channel.purge(limit=num+1)
+    @discord.option(name="刪除訊息數")
+    async def clean(self, ctx, 刪除訊息數: int):
+        await ctx.channel.purge(limit=刪除訊息數+1)
         await ctx.respond("The message has been cleaned^__^")
         time.sleep(3)
 
     
-    @slash_command(name="presence", description="更換目前機器人的動態")
+    @bridge.bridge_command(name="presence", description="更換目前機器人的動態", aliases=["ps"])
     @commands.cooldown(1, 300, commands.BucketType.user)
     async def presence(self, ctx):
         game = random.choice(conf["presence"])
@@ -73,7 +76,6 @@ class User(Cog_Extension):
         print("-------")
         await self.client.change_presence(activity=discord.Game(name=game))
         await ctx.respond(f"{ctx.author.mention}，已經將動態更改為`{game}`，5分鐘後可以再次使用這個指令！")
-
-
+        
 def setup(client):
     client.add_cog(User(client))
