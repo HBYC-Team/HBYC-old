e25cf4cbbffaa5c265fc2932b8420c70b9b70a90
dragonyc1002 <mivck1012@gmail.com>
2022-07-03 20:56:25 +0800

    typo

--- bat/install.bat
+++
@@ -1,5 +0,0 @@
-py3 -m pip install -r ../requirements.txt
-
-echo "Installing the Packges..."
-echo "Please wait for few times..."
-
--- sh/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python -m pip install -r ../requirements.txt
-
-echo "Start installing the packagess"
